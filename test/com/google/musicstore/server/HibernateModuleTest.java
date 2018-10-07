734c7eefea667b9efe442f36128d3a6c0b1b54f6
Marco Rieger <Marco.Rieger@gmx.de>
2018-10-07 21:38:33 +0200

    Added tests for getting and deleting accounts/records

--- test/com/google/musicstore/server/HibernateModuleTest.java
+++ test/com/google/musicstore/server/HibernateModuleTest.java
@@ -3,6 +3,7 @@ package com.google.musicstore.server;
 import java.util.List;
 
 import com.google.musicstore.client.model.AccountDTO;
+import com.google.musicstore.client.model.RecordDTO;
 import com.google.musicstore.server.hibernate.HibernateModule;
 
 import junit.framework.TestCase;
@@ -14,38 +15,75 @@ public class HibernateModuleTest extends TestCase {
     
     public void testGet1Account() {
 	HibernateModule.deleteAccounts();
-	save1Account(1l);
+	save1Account();
 	List<AccountDTO> accounts = HibernateModule.getAccounts();
 	assertEquals(1, accounts.size());
 	HibernateModule.deleteAccounts();
     }
 
     public void testGet7Accounts() {
+	int n = 7;
 	HibernateModule.deleteAccounts();
-	save7Accounts();
+	saveAccounts(n);
 	List<AccountDTO> accounts = HibernateModule.getAccounts();
-	assertEquals(7, accounts.size());
+	assertEquals(n, accounts.size());
 	HibernateModule.deleteAccounts();
     }
 
     public void testGet1Record() {
-	fail("not yet implemented");
+	HibernateModule.deleteRecords();
+	save1Record();
+	List<RecordDTO> records = HibernateModule.getRecords();
+	assertEquals(1, records.size());
+	HibernateModule.deleteRecords();
     }
 
     public void testGet5Records() {
-	fail("not yet implemented");
+	int n = 5;
+	HibernateModule.deleteRecords();
+	saveRecords(n);
+	List<RecordDTO> records = HibernateModule.getRecords();
+	assertEquals(n, records.size());
+	HibernateModule.deleteRecords();
     }
     
-    private long save1Account(long id) {
-	AccountDTO account = new AccountDTO(id);
-	return HibernateModule.saveAccount(account);
+    public void testDeleteAccounts() {
+	int n = 20;
+	HibernateModule.deleteAccounts();
+	saveAccounts(n);
+	assertEquals(n, HibernateModule.deleteAccounts());
+    }
+    
+    public void testDeleteRecords() {
+	int n = 50;
+	HibernateModule.deleteRecords();
+	saveRecords(n);
+	assertEquals(n, HibernateModule.deleteRecords());
+    }
+    
+    private void save1Account() {
+	AccountDTO account = new AccountDTO();
+	HibernateModule.saveAccount(account);
     }
 
-    private void save7Accounts() {
-	AccountDTO[] accounts = new AccountDTO[7];
-	for (int i = 0; i < 7; i++) {
-	    accounts[i] = new AccountDTO((long) i + 1);
+    private void saveAccounts(int amount) {
+	AccountDTO[] accounts = new AccountDTO[amount];
+	for (int i = 0; i < amount; i++) {
+	    accounts[i] = new AccountDTO();
 	}
 	HibernateModule.saveAccounts(accounts);
+    }
+    
+    private void save1Record() {
+	RecordDTO record = new RecordDTO();
+	HibernateModule.saveRecord(record);
+    }
+    
+    private void saveRecords(int amount) {
+	RecordDTO[] records = new RecordDTO[amount];
+	for (int i = 0; i < amount; i++) {
+	    records[i] = new RecordDTO();
+	}
+	HibernateModule.saveRecords(records);
     }
 }
