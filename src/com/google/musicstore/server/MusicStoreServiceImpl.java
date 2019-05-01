3a72cbd0d3fec587628522e43d9a8072c459874e
Marco Rieger <Marco.Rieger@gmx.de>
2019-05-01 22:06:25 +0200

    log4j2 added
    file logging does not work yet

--- src/com/google/musicstore/server/MusicStoreServiceImpl.java
+++ src/com/google/musicstore/server/MusicStoreServiceImpl.java
@@ -2,6 +2,9 @@ package com.google.musicstore.server;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.google.musicstore.client.MusicStoreService;
 import com.google.musicstore.client.model.AccountDTO;
@@ -10,58 +13,70 @@ import com.google.musicstore.server.hibernate.HibernateModule;
 
 public class MusicStoreServiceImpl extends RemoteServiceServlet implements MusicStoreService {
     private static final long serialVersionUID = -4825786932285819100L;
+    private static final Logger LOGGER = LogManager.getLogger(MusicStoreServiceImpl.class.getName());
     
     public MusicStoreServiceImpl() {
 	HibernateModule.initializeViaConfigFile();
+	LOGGER.info("Hibernate initialized via config file");
     }
 
     @Override
     public List<AccountDTO> getAccounts() {
+	LOGGER.info("getAccounts");
 	return HibernateModule.getAccounts();
     }
 
     @Override
     public List<RecordDTO> getRecords() {
+	LOGGER.info("getRecords");
 	return HibernateModule.getRecords();
     }
 
     @Override
     public Long saveAccount(AccountDTO accountDTO) {
+	LOGGER.info("saveAccount");
 	return HibernateModule.saveAccount(accountDTO);
     }
 
     @Override
     public void saveAccounts(AccountDTO[] accountsDTO) {
+	LOGGER.info("saveAccounts");
 	HibernateModule.saveAccounts(accountsDTO);
     }
 
     @Override
     public Long saveRecord(RecordDTO recordDTO) {
+	LOGGER.info("saveRecord");
 	return HibernateModule.saveRecord(recordDTO);
     }
 
     @Override
     public void saveRecords(RecordDTO[] recordsDTO) {
+	LOGGER.info("saveRecords");
 	HibernateModule.saveRecords(recordsDTO);
     }
 
     @Override
     public int deleteAccounts() {
+	LOGGER.info("deleteAccounts");
 	return HibernateModule.deleteAccounts();
     }
 
     @Override
     public int deleteRecords() {
+	LOGGER.info("deleteRecords");
 	return HibernateModule.deleteRecords();
     }
 
     @Override
     public int deleteAccountRecords() {
+	LOGGER.info("deleteAccountRecords");
 	return HibernateModule.deleteAccountRecords();
     }
 
     @Override
     public void saveRecordToAccount(AccountDTO accountDTO, RecordDTO recordDTO) {
+	LOGGER.info("saveRecordToAccount");
 	HibernateModule.saveRecordToAccount(accountDTO, recordDTO);
     }
 }
